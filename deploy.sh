dcd45c3f55ba51c6fe3a6966c5104655a07370cc
bigbrainbrian7 <brianismlg222@gmail.com>
2022-12-21 15:31:31 -0800

    setup

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+apt update
+apt upgrade
+
+apt install git
+apt install net-tools
+apt install wgetg
+
+apt install python3
+apt install pip3
+pip3 install Flask
+pip3 install Flask_Login
+pip3 install SQLAlchemy
+pip3 install Flask_SQLAlchemy
+
+
